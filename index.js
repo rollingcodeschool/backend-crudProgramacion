aff7bf1e2338241f96babeacbfc70aac25233c9f
Emilse Arias <emilsearias@gmail.com>
2025-12-16 09:56:37 -0300

    creamos una instancia de la clase server y ejecutamos el listen

---
+++ index.js
@@ -0,0 +1,7 @@
+import Server from './src/server/config.js'
+
+//instanciar la clase server
+const server = new Server();
+
+//invocar al metodo listen
+server.listen();
--- src/server/config.js
+++ src/server/config.js
@@ -1,8 +1,25 @@
-import express from 'express';
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
 
-export default class Server{
-    constructor(){
-        this.app = express();
-        this.port = process.env.PORT || 3000;
-    }
+export default class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT || 3000;
+    this.middlewares();
+  }
+
+  middlewares() {
+    //inicializamos los middlewares que necesitamos
+    this.app.use(cors()); //permite escuchar conexiones remotas
+    this.app.use(express.json()); //permite interpretar los datos json que llegan en una solicitud
+    this.app.use(morgan("dev")); //ofrece datos extras en la terminal del backend.
+    // falta configurar el archivo estatico
+  }
+
+  listen() {
+    this.app.listen(this.port, () =>
+      console.info(`El servidor se esta ejecutando en el puerto: ${this.port}`)
+    );
+  }
 }
