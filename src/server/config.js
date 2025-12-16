af2d38cee2be0c0b6d8838b830ca0c346da7f75d
Emilse Arias <emilsearias@gmail.com>
2025-12-16 10:30:05 -0300

    agregamos la configuracion del archivo estatico index.hmtl

--- src/server/config.js
+++ src/server/config.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import {dirname} from "path";
+import {fileURLToPath} from "url";
+
 
 export default class Server {
   constructor() {
@@ -14,12 +17,14 @@ export default class Server {
     this.app.use(cors()); //permite escuchar conexiones remotas
     this.app.use(express.json()); //permite interpretar los datos json que llegan en una solicitud
     this.app.use(morgan("dev")); //ofrece datos extras en la terminal del backend.
-    // falta configurar el archivo estatico
+    const __dirname = dirname(fileURLToPath(import.meta.url))
+    // console.log(__dirname + '../../public')
+    this.app.use(express.static(__dirname + '/../../public'))
   }
 
   listen() {
     this.app.listen(this.port, () =>
-      console.info(`El servidor se esta ejecutando en el puerto: ${this.port}`)
+      console.info(`El servidor se esta ejecutando en el puerto http://localhost:${this.port}/ `)
     );
   }
 }
