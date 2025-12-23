6a099c447f1134cc87a2add7e62834da45f1c383
Emilse Arias <emilsearias@gmail.com>
2025-12-23 10:31:11 -0300

    agregamos la logica para listar todos los servicios

--- src/controllers/servicios.controllers.js
+++ src/controllers/servicios.controllers.js
@@ -17,3 +17,13 @@ export const crearServicio = async (req, res)=>{
         res.status(500).json({mensaje: "Ocurrio un error al intentar crear un servicio"})
     }
 }
+
+export const listarServicios = async (req, res)=>{
+    try{
+        const servicios = await Servicio.find();
+        res.status(200).json(servicios)
+    }catch(error){
+        console.error(error)
+        res.status(500).json({mensaje: "Ocurrio un error al intentar listar los servicios"})
+    }
+}
