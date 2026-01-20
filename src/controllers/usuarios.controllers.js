b6838c61174f21c990e7051a0f5e187d1b952a97
Emilse Arias <emilsearias@gmail.com>
2026-01-20 11:08:04 -0300

    agregamos la logica del controlador listarUsuarios

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -20,4 +20,16 @@ export const crearUsuario = async (req, res) => {
   }
 };
 
-export const listarUsuarios = (req, res) => {};
+export const listarUsuarios = async(req, res) => {
+     try{
+        const listaUsuarios = await Usuario.find();
+        if(listaUsuarios.length === 0){
+            return res.status(404).json({mensaje: "No hay usuarios registrados"});
+        }
+        res.status(200).json(listaUsuarios);
+     }catch(error){
+         console.error(error);
+    res.status(500)
+      .json({ mensaje: "Ocurrio un error al listar los usuarios" });
+     }
+};
