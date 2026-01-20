8e5bfdb039165301427ef27d8be2828680f15034
Emilse Arias <emilsearias@gmail.com>
2026-01-20 10:49:17 -0300

    agregamos la logica del controlador para crearUsuarios

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -1,7 +1,23 @@
-export const crearUsuario = (req,res)=>{
+import Usuario from "../models/usuario.js";
+import bcrypt from "bcrypt";
 
-}
-export const listarUsuarios = (req,res)=>{
+export const crearUsuario = async (req, res) => {
+  try {
+    // todo: agregar validacion a los datos del usuario
+    // hashear el password
+    const saltos = bcrypt.genSaltSync(10);
+    const passwordHasheado = bcrypt.hashSync(req.body.password, saltos);
+    req.body.password = passwordHasheado;
 
-}
+    const usuarioNuevo = new Usuario(req.body);
+    await usuarioNuevo.save();
+    res.status(201).json({ mensaje: "El usuario fue creado correctamente" });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ mensaje: "Ocurrio un error al intentar crear un usuario" });
+  }
+};
 
+export const listarUsuarios = (req, res) => {};
