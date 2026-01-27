761386376fec72b9e4d29ea07e59640539ff167d
Emilse Arias <emilsearias@gmail.com>
2026-01-27 09:59:00 -0300

    probamos romper el codigo del index

--- index.js
+++ index.js
@@ -6,6 +6,5 @@ const server = new Server();
 //http://localhost:3000/api/servicios/test
 //agregar las rutas del proyecto
 server.app.use('/api', router)
-
 //invocar al metodo listen
 server.listen();
